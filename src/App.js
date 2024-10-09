7631d3641cc3b80e02ea5111db07aa543abab672
John Doe <johndoe@example.com>
2024-10-09 19:00:51 +0500

    Added the cybersecurity

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <h1 className="App-link">Learn Docker with React.js</h1>
         <h1 className="App-link">Learn Devops Engineering</h1>
         <h1 className="App-link">Cybersecurity</h1>
