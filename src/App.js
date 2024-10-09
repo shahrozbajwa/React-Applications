5e2ae8ff34f0533245fc60117cdaa1bd95a66521
John Doe <johndoe@example.com>
2024-10-09 18:05:16 +0500

    Added the Devops Engineering

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-link">Learn Docker with React.js</h1>
+        <h1 className="App-link">Learn Devops Engineering</h1>
       </header>
     </div>
   );
